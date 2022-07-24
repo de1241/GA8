ad14fcea0022bacb8b153b94cf7878b9f250e32e
de1241 <109894666+de1241@users.noreply.github.com>
2022-07-24 21:27:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+st.write("""
+# Maximum of 3 numbers
+This app predicts the credit card approval probablity
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def user_input_features():
+    number1 = st.number_input("Enter_first_number")
+    number2 = st.number_input("Enter_second_number")
+    number3 = st.number_input("Enter_third_number")
+
+    data = {         
+            'Enter_first_number': number1,
+            'Enter_second_number': number2,
+            'Enter_third_number': number3
+            }
+    Ans = max(number1,number2,number3)
+    return Ans
+
+
+st.subheader('User Input parameters')
+st.write(Ans)
